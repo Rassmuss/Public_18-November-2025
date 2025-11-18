9d849841ce1ff1ca6cb0bccd05e1d24607ce044f
name5-qw <student1001_5@semk.ru>
2025-11-18 16:23:30 +0300

    25w47a

---
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.test2
+
+import android.content.Intent
+import android.media.Image
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val next: Button = findViewById(R.id.button)
+        next.setOnClickListener {
+            val myIntent = Intent(this, snd_nme::class.java)
+            startActivityForResult(myIntent, 0)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/test2/trd_nme.kt
@@ -0,0 +1,20 @@
+package com.example.test2
+
+import android.content.Intent
+import android.media.Image
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class trd_nme : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.trd)
+
+    }
+}
